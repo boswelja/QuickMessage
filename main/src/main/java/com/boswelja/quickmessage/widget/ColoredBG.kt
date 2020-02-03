d0987d85c1a93aaa618541ff997ccf6bb1d100b1
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-02-03 23:17:37 +1300

    Add 2 more widgets, one with a light background and one with a dark background This gives more options than just a giant pink square for a widget

--- main/src/main/java/com/boswelja/quickmessage/widget/ColoredBG.kt
+++ main/src/main/java/com/boswelja/quickmessage/widget/ColoredBG.kt
@@ -1,4 +1,4 @@
-package com.boswelja.quickmessage
+package com.boswelja.quickmessage.widget
 
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
@@ -7,8 +7,10 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.widget.RemoteViews
+import com.boswelja.quickmessage.ActionService
+import com.boswelja.quickmessage.R
 
-class QuickMessageWidget : AppWidgetProvider() {
+class ColoredBG : AppWidgetProvider() {
 
     override fun onUpdate(
         context: Context,
@@ -17,7 +19,8 @@ class QuickMessageWidget : AppWidgetProvider() {
     ) {
         appWidgetIds?.forEach { appWidgetId ->
             val sendMessagePendingIntent = Intent(context, ActionService::class.java).apply {
-                action = ActionService.ACTION_SEND_MESSAGE
+                action =
+                    ActionService.ACTION_SEND_MESSAGE
             }.let {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     PendingIntent.getForegroundService(context, 0, it, 0)
@@ -26,7 +29,9 @@ class QuickMessageWidget : AppWidgetProvider() {
                 }
             }
 
-            val remoteViews = RemoteViews(context.packageName, R.layout.widget_quick_message).apply {
+            val remoteViews = RemoteViews(context.packageName,
+                R.layout.widget_quick_message_coloredbg
+            ).apply {
                 setOnClickPendingIntent(R.id.button, sendMessagePendingIntent)
             }
 
---
+++ main/src/main/java/com/boswelja/quickmessage/widget/DarkBG.kt
@@ -0,0 +1,41 @@
+package com.boswelja.quickmessage.widget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import com.boswelja.quickmessage.ActionService
+import com.boswelja.quickmessage.R
+
+class DarkBG : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray?
+    ) {
+        appWidgetIds?.forEach { appWidgetId ->
+            val sendMessagePendingIntent = Intent(context, ActionService::class.java).apply {
+                action =
+                    ActionService.ACTION_SEND_MESSAGE
+            }.let {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    PendingIntent.getForegroundService(context, 0, it, 0)
+                } else {
+                    PendingIntent.getService(context, 0, it, 0)
+                }
+            }
+
+            val remoteViews = RemoteViews(context.packageName,
+                R.layout.widget_quick_message_darkbg
+            ).apply {
+                setOnClickPendingIntent(R.id.button, sendMessagePendingIntent)
+            }
+
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+        }
+    }
+}
