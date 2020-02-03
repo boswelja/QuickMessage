043ebcbc50bd3180947c6c5731f3850b82299f5b
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-02-03 19:18:12 +1300

    Create a basic widget

---
+++ main/src/main/java/com/boswelja/quickmessage/QuickMessageWidget.kt
@@ -0,0 +1,36 @@
+package com.boswelja.quickmessage
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+
+class QuickMessageWidget : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray?
+    ) {
+        appWidgetIds?.forEach { appWidgetId ->
+            val sendMessagePendingIntent = Intent(context, ActionService::class.java).apply {
+                action = ActionService.ACTION_SEND_MESSAGE
+            }.let {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    PendingIntent.getForegroundService(context, 0, it, 0)
+                } else {
+                    PendingIntent.getService(context, 0, it, 0)
+                }
+            }
+
+            val remoteViews = RemoteViews(context.packageName, R.layout.widget_quick_message).apply {
+                setOnClickPendingIntent(R.id.button, sendMessagePendingIntent)
+            }
+
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+        }
+    }
+}
