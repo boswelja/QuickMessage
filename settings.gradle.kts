7542028750805441ba3d8d31fd36cc0abc82e107
Jack Boswell (boswelja) <boswelja@outlook.com>
2026-03-04 20:42:05 +1100

    Migrate to settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,42 @@
+rootProject.name = "Quick Message"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.android.settings") version "8.12.3"
+}
+
+include(":main")
+
+android {
+    buildToolsVersion = "36.0.0"
+    compileSdk = 36
+    targetSdk = 36
+    minSdk = 29
+}
